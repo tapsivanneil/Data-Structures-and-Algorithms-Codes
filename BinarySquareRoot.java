c07b579c2002fe34e75f4c54beba20f525c26211
tapsivanneil <tapsivanneil@gmail.com>
2023-03-23 09:39:43 +0800

    Fixed Binary Squareroot

--- BinarySquareRoot.java
+++ BinarySquareRoot.java
@@ -1,8 +1,8 @@
 public class BinarySquareRoot {
     public static void main(String[] args) {
         Double list[] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
-        double x = 9;
-        double ep = 0.1;
+        double x = 81;
+        double ep = 0.0001;
         System.out.println(search(list,x,ep));
 
     }
@@ -15,16 +15,14 @@ public class BinarySquareRoot {
         
         for(int i = 0; hi - lo > ep; i++ ){
 
-            double mid = lo + (hi - lo)/2;
+            double mid = lo + (hi - lo)/2.0;
             double sqr = mid * mid;
-
             if (sqr == x) return mid;
-            if (sqr > x) hi = mid - 1;
-            else lo = mid + 1;
+            if (sqr > x) hi = mid;
+            else lo = mid;
 
             System.out.println(mid);
         }
-
         return -1;
 
     }
